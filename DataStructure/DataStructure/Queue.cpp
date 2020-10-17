ff7666d3c5a25fd2468e1edcc61e6c09fc28f432
SEHEEKIM019 <seheekim19@naver.com>
2020-10-18 03:33:55 +0900

    - DataStructure>Queue.cpp

---
+++ DataStructure/DataStructure/Queue.cpp
@@ -0,0 +1,437 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct q_
+{
+	int* queue; //사용자가 메모리 할당할 수 있도록 포인터 변수로 변경
+	int front, rear;
+	int capacity;
+} Queue;
+// 서버(Queue)
+void Push(Queue* q, int data)
+{
+	if ((q->rear + 1) % q->capacity == q->front)
+		return;
+
+	q->rear = (q->rear + 1) % q->capacity;
+	q->queue[q->rear] = data;
+}
+int Pop(Queue* q)
+{
+	if (q->front == q->rear)
+		return 0xfffffff;
+
+	q->front = (q->front + 1) % q->capacity;
+	return q->queue[q->front];
+}
+void InitQueue(Queue* q, int cap)
+{
+	q->queue = (int*)malloc(sizeof(int) * cap);
+	q->capacity = cap;
+	q->front = q->rear = 0;
+}
+void UninitQueue(Queue* q)
+{
+	free(q->queue);
+	q->front = q->rear = 0;
+}
+//////////////////클라이언트
+int main()
+{
+	Queue q1;
+	Queue q2;
+
+	InitQueue(&q1, 100);
+	Push(&q1, 10);
+	Push(&q1, 20);
+	Push(&q1, 30);
+
+	InitQueue(&q2, 500);
+	Push(&q2, 100);
+	Push(&q2, 150);
+
+	printf("queue1 : %d\n", Pop(&q1));
+	printf("queue1 : %d\n", Pop(&q1));
+	printf("queue1 : %d\n", Pop(&q1));
+	UninitQueue(&q1);
+
+	printf("queue2 : %d\n", Pop(&q2));
+	printf("queue2 : %d\n", Pop(&q2));
+	UninitQueue(&q2);
+}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//typedef struct q_
+//{
+//	int* queue;
+//	int front, rear;
+//	int capacity;
+//} Queue;
+//// 서버(Queue)
+//void Push(Queue* q, int data)
+//{
+//	if ((q->rear + 1) % q->capacity == q->front)
+//		return;
+//
+//	q->rear = (q->rear + 1) % q->capacity;
+//	q->queue[q->rear] = data;
+//}
+//int Pop(Queue* q)
+//{
+//	if (q->front == q->rear)
+//		return 0xfffffff;
+//
+//	q->front = (q->front + 1) % q->capacity;
+//	return q->queue[q->front];
+//}
+//void InitQueue(Queue* q, int cap)
+//{
+//	q->queue = (int*)malloc(sizeof(int) * cap);
+//	q->capacity = cap;
+//	q->front = q->rear = 0;
+//}
+//void UninitQueue(Queue* q)
+//{
+//	free(q->queue);
+//	q->front = q->rear = 0;
+//}
+////////////////////클라이언트
+//int main()
+//{
+//	Queue q1;
+//	Queue q2;
+//
+//	InitQueue(&q1, 100);
+//	Push(&q1, 10);
+//	Push(&q1, 20);
+//	Push(&q1, 30);
+//
+//	InitQueue(&q2, 500);
+//	Push(&q2, 100);
+//	Push(&q2, 150);
+//
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	UninitQueue(&q1);
+//
+//	printf("queue2 : %d\n", Pop(&q2));
+//	printf("queue2 : %d\n", Pop(&q2));
+//	UninitQueue(&q2);
+//}
+
+//Queue struct>포인터 queue
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//typedef struct q_
+//{
+//	int* queue;
+//	int front, rear;
+//} Queue;
+//// 서버(Queue)
+//void Push(Queue* q, int data)
+//{
+//	if ((q->rear + 1) % 5 == q->front)
+//		return;
+//
+//	q->rear = (q->rear + 1) % 5;
+//	q->queue[q->rear] = data;
+//}
+//int Pop(Queue* q)
+//{
+//	if (q->front == q->rear)
+//		return 0xfffffff;
+//
+//	q->front = (q->front + 1) % 5;
+//	return q->queue[q->front];
+//}
+//void InitQueue(Queue* q, int cap)
+//{
+//	q->queue = (int*)malloc(sizeof(int)* cap);
+//	q->front = q->rear = 0;
+//}
+//void UninitQueue(Queue* q)
+//{
+//	free(q->queue);
+//	q->front = q->rear = 0;
+//}
+////////////////////클라이언트
+//int main()
+//{
+//	Queue q1;
+//	Queue q2;
+//
+//	InitQueue(&q1,5);
+//	Push(&q1, 10);
+//	Push(&q1, 20);
+//	Push(&q1, 30);
+//
+//	InitQueue(&q2,5);
+//	Push(&q2, 100);
+//	Push(&q2, 150);
+//
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	UninitQueue(&q1);
+//
+//	printf("queue2 : %d\n", Pop(&q2));
+//	printf("queue2 : %d\n", Pop(&q2));
+//	UninitQueue(&q2);
+//}
+
+//Queue 동적 메모리 초기화
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//typedef struct q_
+//{
+//	int queue[5];
+//	int front, rear;
+//} Queue;
+//// 서버(Queue)
+//void Push(Queue* q, int data)
+//{
+//	if ((q->rear + 1) % 5 == q->front)
+//		return;
+//
+//	q->rear = (q->rear + 1) % 5;
+//	q->queue[q->rear] = data;
+//}
+//int Pop(Queue* q)
+//{
+//	if (q->front == q->rear)
+//		return 0xfffffff;
+//
+//	q->front = (q->front + 1) % 5;
+//	return q->queue[q->front];
+//}
+//void InitQueue(Queue* q)
+//{
+//	q->front = q->rear = 0;
+//}
+//void UninitQueue(Queue* q)
+//{
+//	q->front = q->rear = 0;
+//}
+////////////////////클라이언트
+//int main()
+//{
+//	Queue q1;
+//	Queue q2;
+//
+//	InitQueue(&q1);
+//	Push(&q1, 10);
+//	Push(&q1, 20);
+//	Push(&q1, 30);
+//
+//	InitQueue(&q2);
+//	Push(&q2, 100);
+//	Push(&q2, 150);
+//
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	UninitQueue(&q1);
+//
+//	printf("queue2 : %d\n", Pop(&q2));
+//	printf("queue2 : %d\n", Pop(&q2));
+//	UninitQueue(&q2);
+//}
+
+//Queue 구조체
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//typedef struct q_
+//{
+//	int queue[5];
+//	int front, rear;
+//} Queue;
+//// 서버(Queue)
+//void Push(Queue* q, int data)
+//{
+//	if ((q->rear + 1) % 5 == q->front)
+//		return;
+//
+//	q->rear = (q->rear + 1) % 5;
+//	q->queue[q->rear] = data;
+//}
+//int Pop(Queue* q)
+//{
+//	if (q->front == q->rear)
+//		return 0xfffffff;
+//
+//	q->front = (q->front + 1) % 5;
+//	return q->queue[q->front];
+//}
+////////////////////클라이언트
+//int main()
+//{
+//	Queue q1 = { 0 };
+//	Queue q2 = { 0 };
+//
+//	Push(&q1, 10);
+//	Push(&q1, 20);
+//	Push(&q1, 30);
+//
+//	Push(&q2, 100);
+//	Push(&q2, 150);
+//
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//	printf("queue1 : %d\n", Pop(&q1));
+//
+//	printf("queue2 : %d\n", Pop(&q2));
+//	printf("queue2 : %d\n", Pop(&q2));
+//}
+
+//Queue(value by reference)
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//// 서버(Queue)
+//void Push(int queue[], int* pfront, int* prear, int data)
+//{
+//	if ((*prear + 1) % 5 == *pfront)
+//		return;
+//
+//	*prear = (*prear + 1) % 5;
+//	queue[*prear] = data;
+//}
+//int Pop(int queue[], int* pfront, int* prear)
+//{
+//	if (*pfront == *prear)
+//		return 0xfffffff;
+//
+//	*pfront = (*pfront + 1) % 5;
+//	return queue[*pfront];
+//}
+////client
+//int main()
+//{
+//	int queue1[5];
+//	int front1 = 0, rear1 = 0;
+//	int queue2[5];
+//	int front2 = 0, rear2 = 0;
+//
+//	Push(queue1, &front1, &rear1, 10);
+//	Push(queue1, &front1, &rear1, 20);
+//	Push(queue1, &front1, &rear1, 30);
+//
+//	Push(queue2, &front2, &rear2, 100);
+//	Push(queue2, &front2, &rear2, 150);
+//
+//	printf("queue1 : %d\n", Pop(queue1, &front1, &rear1));
+//	printf("queue1 : %d\n", Pop(queue1, &front1, &rear1));
+//	printf("queue1 : %d\n", Pop(queue1, &front1, &rear1));
+//
+//	printf("queue2 : %d\n", Pop(queue2, &front2, &rear2));
+//	printf("queue2 : %d\n", Pop(queue2, &front2, &rear2));
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//// 서버(Queue)
+//void Push(int queue[], int* pfront, int* prear, int data)
+//{
+//	if ((*prear + 1) % 5 == *pfront)
+//		return;
+//
+//	*prear = (*prear + 1) % 5;
+//	queue[*prear] = data;
+//}
+//int Pop(int queue[], int* pfront, int* prear)
+//{
+//	if (*pfront == *prear)
+//		return 0xfffffff;
+//
+//	*pfront = (*pfront + 1) % 5;
+//	return queue[*pfront];
+//}
+////client
+//int main()
+//{
+//	int queue1[5];
+//	int front1 = 0, rear1 = 0;
+//	int queue2[5];
+//	int front2 = 0, rear2 = 0;
+//
+//	Push(queue1, &front1, &rear1, 10);
+//	Push(queue1, &front1, &rear1, 20);
+//	Push(queue1, &front1, &rear1, 30);
+//
+//	Push(queue2, &front2, &rear2, 100);
+//	Push(queue2, &front2, &rear2, 150);
+//
+//	printf("queue1 : %d\n", Pop(queue1, &front1, &rear1));
+//	printf("queue1 : %d\n", Pop(queue1, &front1, &rear1));
+//	printf("queue1 : %d\n", Pop(queue1, &front1, &rear1));
+//
+//	printf("queue2 : %d\n", Pop(queue2, &front2, &rear2));
+//	printf("queue2 : %d\n", Pop(queue2, &front2, &rear2));
+//}
+
+//원형 큐
+
+////server
+//int queue[5];
+//int front = 0, rear = 0;
+//void Push(int data) {    
+//	if ((rear + 1)%5 == front)
+//		return;
+//	rear = (rear + 1) % 5; //최대값 5로 나누면 0,1,2,3,4 반복시킬 수 있음
+//	queue[rear] = data;
+//}
+//int Pop() {
+//	if (front == rear)
+//		return 0xfffffff;
+//	front = (front + 1) % 5;
+//	return queue[front];
+//}
+////client
+//int main() {
+//	Push(10);
+//	Push(20);
+//	Push(30);
+//
+//	printf("%d\n", Pop()); //10
+//	printf("%d\n", Pop()); //20
+//	printf("%d\n", Pop()); //30
+//
+//	Push(10);
+//	Push(20);
+//	Push(30);
+//
+//	printf("%d\n", Pop()); //10
+//	printf("%d\n", Pop()); //20
+//	printf("%d\n", Pop()); //30
+//	return 0;
+//}
+
+//선형 큐(증가만 하다 배열 크기를 벗어나 오류가 발생할 수 있다.)
+
+////server
+//int queue[10];
+//int front = 0, rear = 0;
+//void Push(int data) {
+//	queue[rear++] = data;
+//}
+//int Pop() {
+//	return queue[front++];
+//}
+////client
+//int main() {
+//	Push(10);
+//	Push(20);
+//	Push(30);
+//
+//	printf("%d\n", Pop()); //10
+//	printf("%d\n", Pop()); //20
+//	printf("%d\n", Pop()); //30
+//	return 0;
+//}
--- DataStructure/DataStructure/Stack.cpp
+++ DataStructure/DataStructure/Stack.cpp
@@ -1,29 +1,167 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//stack 동적 메모리 할당, stack 초기화 함수
+
 //server
-int stack[10];
-int top = 0;
-void Push(int data) {
-	stack[top] = data;
-	++top;
+struct Stack
+{
+	//int stack[10];
+	int* stack; //정적 메모리에서 동적 메모리 사용할 수 있도록 변경
+	int top = 0;
+};
+void Push(Stack* st,int data) {
+	//주소를 알고 있으면 멤버를 접근할 때는 화살표 연산자  
+	st->stack[st->top] = data;
+	++st->top;
 }
-int Pop() {
-	--top;
-	return stack[top];
+int Pop(Stack* st) {
+	--st->top;
+	return st->stack[st->top];
 }
-//client 
+//void InitStack(Stack* st) {
+//	st->top = 0;
+//}
+void InitStack(Stack* st, int cap){//cap: 할당 용량
+	st->stack = (int*)malloc(sizeof(int)*cap);
+	st->top = 0;
+}
+//void UninitStack(Stack* st) {
+//	st->top = 0;
+//}
+void UninitStack(Stack* st,int cap) {
+	free(st->stack);
+	st->top = 0;
+}
+//client  
 int main() {
-	Push(10);
-	Push(20);
-	Push(30);
 	
-	int data;
-	data = Pop();
-	printf("%d\n", data);
-	data = Pop();
-	printf("%d\n", data);
-	data = Pop();
-	printf("%d\n", data);
+	Stack st; //Stack st={0}; initStack으로 초기화하여 선언 때 초기화 필요 x
+	Stack st2;
+
+	//InitStack(&st); //Stack 초기화 인터페이스
+	InitStack(&st,500); //heap 메모리 500byte 할당
+	Push(&st, 10); 
+	Push(&st, 20);
+	Push(&st, 30);
+	printf("%d\n", Pop(&st)); //30
+	printf("%d\n", Pop(&st)); //20
+	printf("%d\n", Pop(&st)); //10 
+	//UninitStack(&st);
+	UninitStack(&st, 500); 
+
+	//InitStack(&st2);
+	InitStack(&st2, 10);
+	Push(&st2, 100);
+	Push(&st2, 200);
+	Push(&st2, 300);
+	printf("%d\n", Pop(&st2)); //300 
+	printf("%d\n", Pop(&st2)); //200 
+	printf("%d\n", Pop(&st2)); //10 
+	//UninitStack(&st2);
+	UninitStack(&st2, 10);
+
 	return 0;
 }
+
+//구조체를 이용한 stack
+
+////server
+//struct Stack
+//{
+//	int stack[10];
+//	int top = 0;
+//};
+//void Push(Stack* st,int data) { //void Push(int Stack[],int *top ,int data) {
+//	//주소를 알고 있으면 멤버를 접근할 때는 화살표 연산자  
+//	st->stack[st->top] = data;  //Stack[*top] = data;
+//
+//	//함수 내 top 증감 내용이 client top 변수에도 적용되어야 하기 때문에 
+//	//call by reference로 int형 포인터 사용
+//	++st->top; //++*top;
+//}
+//int Pop(Stack* st) {
+//	--st->top;
+//	return st->stack[st->top];
+//}
+////client  
+//int main() {
+//	
+//	Stack st; //Stack st={0}; 쌤은 0으로 초기화해야 되던데 나는 초기화하면 안됨...
+//	Stack st2;
+//	Push(&st, 10);
+//	Push(&st, 20);
+//	Push(&st, 30);
+//
+//	printf("%d\n", Pop(&st)); //30
+//	printf("%d\n", Pop(&st)); //20
+//	printf("%d\n", Pop(&st)); //10 
+//
+//	Push(&st2, 100);
+//	Push(&st2, 200);
+//	Push(&st2, 300);
+//
+//	printf("%d\n", Pop(&st2)); //300 
+//	printf("%d\n", Pop(&st2)); //200 
+//	printf("%d\n", Pop(&st2)); //10 
+//
+//	return 0;
+//}
+
+//stack basel(printf("%d\n", Pop()))
+
+////server
+//int stack[10];
+//int top = 0;
+//void Push(int data) {
+//	stack[top] = data;
+//	++top;
+//}
+//int Pop() {
+//	--top;
+//	return stack[top];
+//}
+////client  
+//int main() {
+//	Push(10);
+//	Push(20);
+//	Push(30);
+//
+//	printf("%d\n", Pop()); //30
+//	Push(40);
+//	Push(50);
+//	printf("%d\n", Pop()); //50
+//	printf("%d\n", Pop()); //40
+//	printf("%d\n", Pop()); //20
+//	printf("%d\n", Pop()); //10 
+//	return 0;
+//}
+
+//stack basel(data=pop())
+
+////server
+//int stack[10];
+//int top = 0;
+//void Push(int data) {
+//	stack[top] = data;
+//	++top;
+//}
+//int Pop() {
+//	--top;
+//	return stack[top];
+//}
+////client 
+//int main() {
+//	Push(10);
+//	Push(20);
+//	Push(30);
+//	
+//	int data;
+//	data = Pop();
+//	printf("%d\n", data);
+//	data = Pop();
+//	printf("%d\n", data);
+//	data = Pop();
+//	printf("%d\n", data);
+//	return 0;
+//}
